f782c8a287b795f8771c0028a449630ef9999d54
jiyoung0123 <taebin100@gmail.com>
2023-04-19 13:08:54 +0900

    0419

---
+++ src/main/java/com/kbstar/Web01Application.java
@@ -0,0 +1,24 @@
+package com.kbstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@SpringBootApplication
+public class Web01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web01Application.class, args);
+	}
+
+	//JSP Setting(이 디렉토리에 JSP를 만들겠다는 뜻)
+	@Bean
+	public InternalResourceViewResolver setupViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+
+}
